0bb0e4ccc4874c81e8807c28a7f03bc843cd6ade
Lyra Freund <lyrafreund@gmail.com>
2023-10-23 16:38:46 -0600

    completed lab that works correctly but doesn't pass pytest -x

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
 
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
 def return_evens(num_list):
-    pass
+    new_list = []
+    new_list = [ n for n in num_list if not n % 2]
+    
+    return print(new_list)
+
+return_evens(numbers)
+
+words = (["Hello", "I'm doing great", "Python is fun"])
 
 def make_exclamation(sentence_list):
-    pass
+    exclaim_list = []
+    exclaim_list = [ n + "!" for n in sentence_list ]
+    return print(exclaim_list)
+
+make_exclamation(words)
